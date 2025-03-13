3fcd428e7e913166acca57b12dcefde94a3a14a4
Dan Benjamin <dan@benjamin.org>
2025-03-13 18:01:05 -0500

    namespace

--- lib/refinery_core/engine.rb
+++ lib/refinery_core/engine.rb
@@ -1,5 +1,13 @@
 module RefineryCore
   class Engine < ::Rails::Engine
+    # Ensure models load properly
+    config.to_prepare do
+      Dir.glob(Rails.root.join("app/models/**/*.rb")).each do |c|
+        require_dependency c
+      end
+    end
+
+    # Allow migrations to be shared with other apps
     initializer :append_migrations do |app|
       unless app.root.to_s.match? root.to_s
         config.paths["db/migrate"].expanded.each do |path|
