2bcf80e43ee2c21ef2c2872b625cc7f5c77e4acd
Dan Benjamin <dan@benjamin.org>
2025-03-13 17:48:29 -0500

    namespace

--- lib/refinery_core/engine.rb
+++ lib/refinery_core/engine.rb
@@ -1,7 +1,5 @@
 module RefineryCore
   class Engine < ::Rails::Engine
-    isolate_namespace RefineryCore
-
     initializer :append_migrations do |app|
       unless app.root.to_s.match? root.to_s
         config.paths["db/migrate"].expanded.each do |path|
